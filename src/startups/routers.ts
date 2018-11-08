fb0caae96abd08b2cdf591e9675df142445e22bd
karol.nadratowski <karol.nadratowski@brightinventions.pl>
2018-11-08 22:32:51 +0100

    Delete genres from project

--- src/startups/routers.ts
+++ src/startups/routers.ts
@@ -1,7 +1,6 @@
 import player from "../routes/player";
 import { Express } from "express";
 import express from "express";
-import genre from "../routes/genres";
 import auth from "../routes/auth";
 import user from "../routes/users";
 import team from "../routes/team";
@@ -9,7 +8,6 @@ import team from "../routes/team";
 function addRouters(app: Express) {
   app.use(express.json());
   app.use("/api/auth", auth);
-  app.use("/api/genres", genre);
   app.use("/api/players", player);
   app.use("/api/teams", team);
   app.use("/api/users", user);
