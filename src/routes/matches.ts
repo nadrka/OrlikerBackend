181b00ac7d0cd2dc6612ee85ef6d57cbbbed5c17
karol.nadratowski <karol.nadratowski@brightinventions.pl>
2018-11-08 22:42:01 +0100

    Add match service and router with routes

---
+++ src/routes/matches.ts
@@ -0,0 +1,14 @@
+import { Response } from "express-serve-static-core";
+import express, { Request } from "express";
+
+const router = express.Router();
+
+router.post("/", async (req: Request, res: Response) => {});
+router.post("/:id/result", async (req: Request, res: Response) => {});
+router.post("/:id/statistics", async (req: Request, res: Response) => {});
+router.get("/:id", async (req: Request, res: Response) => {});
+router.put("/:id", async (req: Request, res: Response) => {});
+router.put("/:id/result", async (req: Request, res: Response) => {});
+router.delete("/:id", async (req: Request, res: Response) => {});
+
+export default router;
--- src/startups/routers.ts
+++ src/startups/routers.ts
@@ -4,10 +4,12 @@ import express from "express";
 import auth from "../routes/auth";
 import user from "../routes/users";
 import team from "../routes/team";
+import matches from "../routes/matches";
 
 function addRouters(app: Express) {
   app.use(express.json());
   app.use("/api/auth", auth);
+  app.use("/api/matches", matches);
   app.use("/api/players", player);
   app.use("/api/teams", team);
   app.use("/api/users", user);
