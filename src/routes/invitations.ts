353e38d4ef35db22262fd036d72015129d420f55
karol.nadratowski <karol.nadratowski@brightinventions.pl>
2018-11-23 23:01:33 +0100

    Add invitations router

---
+++ src/routes/invitations.ts
@@ -0,0 +1,9 @@
+import { Response } from "express";
+import { Request } from "express";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", async (req: Request, res: Response) => {});
+
+export default router;
--- src/startups/routers.ts
+++ src/startups/routers.ts
@@ -5,11 +5,13 @@ import auth from "../routes/auth";
 import user from "../routes/users";
 import team from "../routes/team";
 import matches from "../routes/matches";
+import invitations from "../routes/invitations";
 import League from "../routes/league";
 
 function addRouters(app: Express) {
   app.use(express.json());
   app.use("/api/auth", auth);
+  app.use("/api/invitations", invitations);
   app.use("/api/leagues", League);
   app.use("/api/matches", matches);
   app.use("/api/players", player);
