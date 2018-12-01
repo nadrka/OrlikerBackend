99b7f67c10bf739b8a765a558b20a191b385df42
karol.nadratowski <karol.nadratowski@brightinventions.pl>
2018-12-01 18:48:29 +0100

    WIP

--- src/routes/league.ts
+++ src/routes/league.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import LeagueService from "../services/leagueService";
 import PlayerStatisticService from "../services/playerStatisticService";
+import MatchService from "../services/matchService";
 
 const router = express.Router();
 const leagueService = new LeagueService();
 const playerStatisticsService = new PlayerStatisticService();
+const matchesService = new MatchService();
 router.post("/", async (req: Request, res: Response) => {
   let league = await leagueService.createLeague(req, res);
   res.send(league);
@@ -14,11 +16,19 @@ router.get("/:id/statistics", async (req: Request, res: Response) => {
   res.send(statistics);
 });
 router.get("/:id/teams", async (req: Request, res: Response) => {
-  const teams = await leagueService.getTeamsFromGivenLeague(req.params.id,res);
+  const teams = await leagueService.getTeamsFromGivenLeague(req.params.id, res);
   res.send(teams);
 });
-router.get("/:id/matches/upcoming", async (req: Request, res: Response) => {});
-router.get("/:id/matches/played", async (req: Request, res: Response) => {});
+router.get("/:id/matches/upcoming", async (req: Request, res: Response) => {
+  const matches = await matchesService.getUpcomingMatchesForLeague(
+    req.params.id
+  );
+  res.send(matches);
+});
+router.get("/:id/matches/played", async (req: Request, res: Response) => {
+  const matches = await matchesService.getPlayedMatchesForLeague(req.params.id);
+  res.send(matches);
+});
 router.get("/:id/matches/all", async (req: Request, res: Response) => {});
 router.get("/:id/teams", async (req: Request, res: Response) => {});
 router.get("/", async (req: Request, res: Response) => {
